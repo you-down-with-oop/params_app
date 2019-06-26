d908ad3d82fd6817c62fdff2431c6c7da545e2ec
Peter Jang <peter.x.jang@gmail.com>
2019-06-26 18:32:46 -0500

    Add url segment guess number example

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     get "/query_guess_url" => "params_examples#query_guess_method"
 
     get "/segment_params_url/:the_variable_part" => "params_examples#segment_params_method"
+    get "/segment_guess_url/:the_users_guess" => "params_examples#query_guess_method"
   end
 end
