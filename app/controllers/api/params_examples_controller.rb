73daa50862240cdccc7f7a3df4867c7587ef30bf
Peter Jang <peter.x.jang@gmail.com>
2019-06-25 20:48:12 -0500

    Add example query params action

--- app/controllers/api/params_examples_controller.rb
+++ app/controllers/api/params_examples_controller.rb
@@ -1,2 +1,8 @@
 class Api::ParamsExamplesController < ApplicationController
+  def query_params_method
+    @message1 = params["my_message"]
+    @message2 = params["second_message"]
+    @combined_message = "You said these two things: #{@message1} and #{@message2}"
+    render "query_params.json.jb"
+  end
 end
